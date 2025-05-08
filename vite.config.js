e74aba846d68d24dae0c0f895704556875fad572
Nishil005 <132841766+Nishil005@users.noreply.github.com>
2025-05-08 22:20:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base : "/nimo-portfolio",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src")
     },
   },
 });
